499eac3bea7647451f91871cff4bec1579e2fe78
Emma Paris <jasonyyeung@gmail.com>
2020-10-08 15:48:21 -0400

    Added rabi time
    Imagine memorizing time zones

--- bot.py
+++ bot.py
@@ -5,8 +5,12 @@ import discord
 import random
 import os
 import psycopg2
+from datetime import datetime
+from pytz import timezone 
+import asyncio
 from discord.ext import commands
 from rabi import Rabi
+#from arabi import Arabi
 import urllib.parse as urlparse
 
 bot = commands.Bot(command_prefix = 'rabi ')
@@ -26,7 +30,6 @@ async def on_message(message):
         ## Misc. commands
         await set_reaction(message)
         await hit_rabi(message)
-        #await ssb_user(message)
         await detect_keywords(message)
         
         await bot.process_commands(message)
@@ -36,6 +39,7 @@ async def on_message(message):
 async def postgres_connect():
     
     url = urlparse.urlparse(os.environ.get('DATABASE_URL'))
+    ##url = urlparse.urlparse(Arabi.DATABASE_URL)
     dbname = url.path[1:]
     user = url.username
     password = url.password
@@ -197,14 +201,103 @@ async def set_reaction(message):
 async def hit_rabi(message):
     if message.content.lower() == 'hit rabi' or message.content.lower() == 'hit ravi':
         value = random.random()
-        print(value)
+
         if value < 0.2:
             await message.channel.send('COUNTER ATTAC')
             await message.channel.send('<:rabiflame:648713302360326148>')
         else:
             await message.channel.send('<:rabicry:650976531354615819>')
-            
+
+
+## I love gw
+@bot.command()
+async def gw(ctx):
+    
+    ## Make sure its gw day
+    weekday = datetime.datetime.now().weekday()
+    hour = datetime.datetime.now().hour
+    if (weekday in [0, 2, 4] and hour >= 6) or (weekday in [1, 3, 5] and hour < 6):
+        
+        ## Find all channels with members in it
+        channels = []
+        for c in ctx.guild.channels:
+            if str(c.type) == "voice" and len(c.members) > 0:
+                channels.append(c)
+
+        ## Tag all members in channel randomly
+        if len(channels) > 0:
+            dialogue = "I love gw.\n"
+            members = channels[0].members
+            for member in range(len(channels[0].members)):
+                tagged = random.choice(members)
+                dialogue = dialogue + str(member + 1) + ". " + tagged.mention
+
+                if str(tagged) == 'Snow#9697' and member == len(channels[0].members) - 1:
+                    dialogue = dialogue + " " + '<:rabidrink:665760786462933012>'
+
+                dialogue = dialogue + "\n"
+                members.remove(tagged)
+
+            ## Send message in 1 block
+            await ctx.send(dialogue)
+
+        else:
+            await ctx.send('No one wants to do gw right now.')
+            await ctx.send('<:rabisad:650976531354615819>')
+
+    else:
+        await ctx.send("There's no gw today.")
+    
+
+## For adding/updating builds (or any screenshot really)
+@bot.command()
+async def add(ctx):
+    return
+    
+
+## For removing builds (or any screenshot really)
+@bot.command()
+async def remove(ctx):
+    return
+
+
+## Imagine memorizing time zones
+@bot.command()
+async def time(ctx):
+    
+    chow = datetime.now(timezone('Australia/Sydney'))
+    steph = datetime.now(timezone('Australia/Brisbane'))
+    utc = datetime.now(timezone('UTC'))
+    edt = datetime.now(timezone('Canada/Eastern'))
+    
+    await ctx.send("Chow: " + chow.strftime('%#I:%M %p') + "\n" +
+                   "Steph: " + steph.strftime('%#I:%M %p') + "\n" +
+                   "UTC: " + utc.strftime('%#I:%M %p') + "\n" +
+                   "Rabi: " + edt.strftime('%#I:%M %p') + "\n")
+    
+
+
+## Sends an automatic message at regular intervals
+##async def background_task():
+##    
+##    await bot.wait_until_ready()
+##    counter = 0
+##    channel = bot.get_channel(590177033586475008) # For now it's just rule, may implement adding server IDs to Heroku later
+##    
+##    ## Check for gw
+##    weekday = datetime.datetime.now().weekday()
+##    hour = datetime.datetime.now().hour
+##    #if (weekday in [0, 2, 4] and hour >= 6) or (weekday in [1, 3, 5] and hour < 6):
+##    while not bot.is_closed():
+##        counter += 1
+##        await channel.send(f'{str(counter)} seconds have passed.')
+##        await asyncio.sleep(10)
+    
+
+## Background checks
+#bot.loop.create_task(background_task())
+    
 
 ## Run
 bot.run(os.environ.get('TOKEN'))
-##bot.run(Rabi.TOKEN)
+##bot.run(Arabi.TOKEN)
--- rabi.py
+++ rabi.py
@@ -1,6 +1,10 @@
 
 class Rabi:
 
+    RATE_ERROR = '**Subs:** atk / def / hp / speed / critc / critd / eff / res\n\n'\
+                 '**Usage:** `rabi rate [level] [colour] [value1] [stat1] ... [value4] [stat4]`\n'\
+                 '> *eg. rabi rate +15 red 9% critc 17% atk 4 spd 80 atk*'
+
     REACTIONS = {
         "fucking ssb users": ["ssb", "seaside bellona"],
         "fucking arby users": ["avild", "avildred", "a. vildred", "a. vild", "a.vildred", "a.vild", "arby"],
@@ -11,11 +15,51 @@ class Rabi:
         "https://saucer.s-ul.eu/yGF94EzV.jpg": ["+15"]
         }
 
-    COMMANDS = ['hi rabi', 'kill rabi', 'hit rabi', 'ssbuser?']
+    COMMANDS = ['hi rabi', 'kill rabi', 'hit rabi', 'ssbuser?', 'rabi rate', 'rabi gw', 'rabi time']
 
     RABI = {
         "<:rabi:646666830651326464><:arabi:648411271334461449>": ['double ravi', 'double rabi', 'rabis', 'ravis'],
         "<:rabi:646666830651326464>": ['ravi', 'rabi'],
         "<:arabi:648411271334461449>": ['mlravi', 'ml ravi', 'ml rabi', 'mlrabi', 'aravi', 'arabi', 'a.rabi', 'a.ravi']
         }
-        
+
+    SUBSTATS = {
+        "atk%": [4, 8],
+        "atk": [35, 46],
+        "def%": [4, 8],
+        "def": [25, 40],
+        "hp%": [4, 8],
+        "hp": [150, 190],
+        "speed": [1, 4],
+        "critc%": [3, 5],
+        "critd%": [3, 7],
+        "eff%": [4, 8],
+        "res%": [4, 8]
+        }
+
+    COLOURS = {
+        "blue": 2,
+        "purple": 3,
+        "red": 4
+        }
+
+    REFORGE = {
+        "atk%": [1, 3, 4, 5, 7, 8],
+        "atk": [],
+        "def%": [1, 3, 4, 5, 7, 8],
+        "def": [],
+        "hp%": [1, 3, 4, 5, 7, 8],
+        "hp": [],
+        "speed": [0, 1, 2, 3, 4, 4],
+        "critc%": [1,  2, 3, 4, 5, 6],
+        "critd%": [1, 2, 3, 4, 6, 7],
+        "eff%": [1, 3, 4, 5, 7, 8],
+        "res%": [1, 3, 4, 5, 7, 8]
+        }
+
+    RATINGS = ["Your poor units...",
+               "It could be better...",
+               "This should get the job done",
+               "<:rabigoodgood:656875318191194124>",
+               "Rabi wants that gear",
+               "<:ban:663121335190552576>"]
